09e55f72b0d5279669cc5e703beca7b3b2f913f6
Crowdbotics <team@crowdbotics.com>
2020-06-22 02:13:13 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/youth_of_love_in_ch_18309/MainActivity.java
@@ -0,0 +1,15 @@
+package com.youth_of_love_in_ch_18309;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "youth_of_love_in_ch_18309";
+    }
+}
---
+++ backend/youth_of_love_in_ch_18309/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for youth_of_love_in_ch_18309 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youth_of_love_in_ch_18309.settings')
+
+application = get_wsgi_application()
